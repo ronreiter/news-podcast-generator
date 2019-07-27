af8a0e3ce44f1e5e3c9eaecd72edfdb56af6df69
ron <ron.reiter@gmail.com>
2019-07-27 10:20:53 +0300

    intro and goodbye

---
+++ README.md
@@ -0,0 +1,36 @@
+# Hacker News Podcast Generator
+
+This application creates a Hacker News Podcast by taking the best links of any day on Hacker News, summarizing the article text
+within the links using an API, and then synthesizing the text to speech using Google's new WaveNet-based Text-To-Speech API.
+
+It will then upload the podcasts to a Google Cloud Storage bucket and create a podcast RSS feed for you, so you can submit it to iTunes or Google Play.
+
+With little modifications, you can use this script to summarize any news website or aggregator, reddit, etc.
+
+### Instructions
+
+1. Copy config.py.example to config.py, and fill in the details.
+AYLIEN_API_ID and AYLIEN_API_KEY are provided when creating a new Aylien account.
+The Google API key and service account json file can be created from the cloud console.
+You will also need to create a Google Storage bucket to store the podcasts and RSS file into.
+
+2. Enable Cloud Text-To-Speech using the Google Cloud console.
+
+3. Install a new virtualenv and run
+
+    pip install -r requirements.txt
+
+3. Run hn.py with the specific date (in ISO format) you'd like to create the podcast for. For example:
+
+    python hn.py --date=2019-07-26
+
+If you do not specify a date, it will use yesterday's date to generate the podcast.
+
+### Useful links
+
+* Audio Library: https://www.youtube.com/audiolibrary/music?ar=3
+
+### TODO:
+
+* Use the google API client library instead of sending an HTTP request
+* Use it on Reddit :)
--- hn.py
+++ hn.py
@@ -1,8 +1,12 @@
+import argparse
 import codecs
 import datetime
+import glob
 import hashlib
 import json
 import os
+import re
+
 import pydub
 
 import requests
@@ -16,16 +20,17 @@ from google.cloud import storage
 
 from config import *
 
+# TODO: use the google API client library instead of sending an HTTP request
 # https://developers.google.com/analytics/devguides/config/mgmt/v3/quickstart/service-py
 # from apiclient.discovery import build
 # from oauth2client.service_account import ServiceAccountCredentials
 # credentials = ServiceAccountCredentials.from_json_keyfile_name("creds.json", 'https://texttospeech.googleapis.com/v1beta1/text:synthesize')
 # service = build('texttospeech', 'v3', credentials)
 
-
 # constants
 TTS_URL = "https://texttospeech.googleapis.com/v1beta1/text:synthesize"
 BEST_STORIES_API = "https://hacker-news.firebaseio.com/v0/beststories.json"
+STORIES_FOR_DATE_PAGE = "https://news.ycombinator.com/front?day=%s"
 STORY_API = "https://hacker-news.firebaseio.com/v0/item/%s.json"
 VOICE_TYPE = 'en-US-WaveNet-D'
 VOICE_GENDER = 'MALE'
@@ -33,8 +38,12 @@ VOICE_LANG = 'en-us'
 TOTAL_SENTENCES = 2
 NUMBER_ARTICLES = 30
 TEMPLATE_FOLDER = "templates"
+PODCASTS_FOLDER = "podcasts"
+NEWS_DATA_FOLDER = "news_data"
+TEMP_FOLDER = "temp"
 
 storage_client = storage.Client.from_service_account_json(SERVICE_ACCOUNT_CREDS)
+t = jinja2.FileSystemLoader(TEMPLATE_FOLDER)
 
 def upload_blob(bucket_name, blob_name, data):
     bucket = storage_client.get_bucket(bucket_name)
@@ -77,15 +86,23 @@ def ssml_to_audio(ssml, format='OGG_OPUS', voice_type=VOICE_TYPE, voice_gender=V
 
     return raw_data
 
-def get_best_hn_urls(num=10):
-    top_items = requests.get(BEST_STORIES_API).json()
+def get_best_hn_urls(num=10, news_date=None):
+    if news_date:
+        html_data = requests.get(STORIES_FOR_DATE_PAGE % news_date).content
+        top_items = re.findall(b"<tr class='athing' id='(\d+)'>", html_data)
+        top_items = [int(x) for x in top_items]
+    else:
+        top_items = requests.get(BEST_STORIES_API).json()
+
     links = []
     for item in top_items[:num]:
+        logging.info('reading item %s', STORY_API % item)
         item_data = requests.get(STORY_API % item).json()
         if 'url' in item_data:
             links.append(item_data['url'])
 
     return links
+
 
 def get_news_data(urls):
     client = textapi.Client(AYLIEN_API_ID, AYLIEN_API_KEY)
@@ -109,7 +126,7 @@ def get_news_data(urls):
     return out
 
 def single_file():
-    news_template = jinja2.FileSystemLoader(TEMPLATE_FOLDER).load(jinja2.Environment(), "news.ssml")
+    news_template = t.load(jinja2.Environment(), "news.ssml")
     ssml = news_template.render(news_data=news_data)
     raw_data = ssml_to_audio(ssml)
 
@@ -117,26 +134,16 @@ def single_file():
         f.write(raw_data)
 
 
-if __name__ == '__main__':
-    today = datetime.date.today().isoformat()
-    news_file = 'news_data/news_data_%s.json' % today
-
-    logging.info('getting news data...')
-    if not os.path.exists(news_file):
-        news_data = get_news_data(get_best_hn_urls(NUMBER_ARTICLES))
-        json.dump(news_data, open(news_file, "w"))
-    else:
-        news_data = json.load(open(news_file))
-
-    item_template = jinja2.FileSystemLoader(TEMPLATE_FOLDER).load(jinja2.Environment(), "item.ssml")
+def generate_podcast(news_items, podcast_fn):
+    item_template = t.load(jinja2.Environment(), "item.ssml")
 
     items = []
 
-    for item in news_data:
+    for item in news_items:
         logging.info('processing %s' % item['title'])
         item_ssml = item_template.render(item)
         item_hash = hashlib.md5(item_ssml.encode('utf8')).hexdigest()
-        blob_name = 'temp/%s.wav' % item_hash
+        blob_name = os.path.join(TEMP_FOLDER, '%s.wav' % item_hash)
 
         if not os.path.exists(blob_name):
             raw_data = ssml_to_audio(item_ssml, format='LINEAR16')
@@ -148,17 +155,68 @@ if __name__ == '__main__':
 
     final = pydub.AudioSegment.empty()
 
-    final += pydub.AudioSegment.from_mp3('resources/main.mp3')
+    final += pydub.AudioSegment.from_mp3('resources/intro.wav')
     for item in items:
-        final += pydub.AudioSegment.from_mp3('resources/interim.mp3')
+        final += pydub.AudioSegment.from_mp3('resources/Everythings_Nice_Sting.mp3')
         final += pydub.AudioSegment.from_wav(item)
 
-    final += pydub.AudioSegment.from_mp3('resources/main.mp3')
+    final += pydub.AudioSegment.from_mp3('resources/goodbye.wav')
 
-    fn = "bestofhn_%s.mp3" % today
-    logging.info('saving %s' % fn)
-    final.export(os.path.join('podcasts', fn), format="mp3")
+    logging.info('saving %s' % podcast_fn)
+    final.export(podcast_fn, format="mp3")
 
     logging.info('uploading...')
-    upload_file(BUCKET_NAME, fn, fn)
+    upload_file(BUCKET_NAME, podcast_fn, podcast_fn)
 
+
+def generate_rss_feed():
+    items = []
+    for f in reversed(sorted(glob.glob(os.path.join(PODCASTS_FOLDER, "bestofhn_*.mp3")))):
+        podcast_date = datetime.datetime.strptime(os.path.basename(f)[9:19], "%Y-%m-%d")
+        items.append({
+            'filename': os.path.basename(f),
+            'size': os.stat(f).st_size,
+            'duration': int(pydub.AudioSegment.from_mp3(f).duration_seconds),
+            'date': podcast_date.strftime("%c"),
+            'nice_date': podcast_date.strftime("%B %-d, %Y"),
+        })
+
+    item_template = t.load(jinja2.Environment(), "podcast.rss")
+
+    return item_template.render(items=items)
+
+
+if __name__ == '__main__':
+    # create directories
+    for d in [PODCASTS_FOLDER, NEWS_DATA_FOLDER, TEMP_FOLDER]:
+        if not os.path.exists(d):
+            os.mkdir(d)
+
+    # generate resources
+    if not os.path.exists("resources/intro.wav"):
+        open("resources/intro.wav", "wb").write(ssml_to_audio(t.load(jinja2.Environment(), "intro.ssml").render(), format='LINEAR16'))
+    if not os.path.exists("resources/goodbye.wav"):
+        open("resources/goodbye.wav", "wb").write(ssml_to_audio(t.load(jinja2.Environment(), "goodbye.ssml").render(), format='LINEAR16'))
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--date", default=(datetime.date.today() - datetime.timedelta(days=1)).isoformat())
+    args = parser.parse_args()
+
+    news_date = args.date
+    news_file = os.path.join(NEWS_DATA_FOLDER, 'news_data_%s.json' % news_date)
+
+    logging.info('getting news data...')
+    if not os.path.exists(news_file):
+        news_data = get_news_data(get_best_hn_urls(NUMBER_ARTICLES, news_date))
+        json.dump(news_data, open(news_file, "w"))
+    else:
+        news_data = json.load(open(news_file))
+
+    fn = os.path.join(PODCASTS_FOLDER, "bestofhn_%s.mp3" % news_date)
+
+    if not os.path.exists(fn):
+        generate_podcast(news_data, fn)
+
+    rss_feed = generate_rss_feed()
+    print(rss_feed)
+    upload_blob(BUCKET_NAME, 'bestofhn.rss', rss_feed)
