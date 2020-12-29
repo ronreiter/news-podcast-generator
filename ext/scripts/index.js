31817aa15c1fc79b3e779429803f1350d1659b02
ronreiter <ron.reiter@gmail.com>
2020-12-29 10:57:26 +0200

    ext

--- ext/scripts/index.js
+++ ext/scripts/index.js
@@ -2,15 +2,24 @@ window.onload = function() {
     let app = new Vue({
         el: '#app',
         data: {
-            articles: []
+            articles: [],
+            largeMode: true,
+            readingList: [],
+            page: "main"
         },
         async mounted() {
             let res = await fetch(`https://bestofhn.storage.googleapis.com/news/bing_ScienceAndTechnology.json`)
             let data = await res.json()
             this.articles = data.value
+            if (localStorage.getItem("largeMode")) {
+                this.largeMode = JSON.parse(localStorage.getItem("largeMode"))
+            }
+            if (localStorage.getItem("readingList")) {
+                this.readingList = JSON.parse(localStorage.getItem("readingList"))
+            }
         },
         methods: {
-            timeSince: (date) => {
+            timeSince: function(date) {
 
                 var seconds = Math.floor((new Date() - new Date(date)) / 1000);
 
@@ -36,7 +45,33 @@ window.onload = function() {
                     return Math.floor(interval) + " minutes";
                 }
                 return Math.floor(seconds) + " seconds";
+            },
+            addToReadingList: function(article) {
+                this.articles.splice(this.articles.indexOf(article), 1)
+                for (let readingListItem of this.readingList) {
+                    if (readingListItem.url === article.url) {
+                        return
+                    }
+                }
+                this.readingList.push(article)
+            },
+            removeFromReadingList: function(article) {
+                for (let i = 0; i < this.readingList.length; i++) {
+                    if (this.readingList[i].url === article.url) {
+                        this.readingList.splice(i, 1)
+                    }
+                }
+                return true
+            },
+        },
+        watch: {
+            largeMode: function(newVal, oldVal) {
+                localStorage.setItem("largeMode", JSON.stringify(newVal))
+            },
+            readingList: function(newVal, oldVal) {
+                localStorage.setItem("readingList", JSON.stringify(newVal))
             }
+
         }
     })
 }
